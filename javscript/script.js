12bb12414d5d57104bab986a782266bc38e28fc3
Shaswata Ghosh <shaswataghosh091@gmail.com>
2026-01-09 03:26:49 +0530

    Add files via upload

---
+++ javscript/script.js
@@ -0,0 +1,57 @@
+console.log("JavaScript is running");
+// let x=5;
+// console.log(x);
+// x=x+3;
+// console.log(x);
+
+// let a=10;
+// console.log(a);
+// a=a*2;
+// console.log(a);
+
+let x=5;
+x=6;
+console.log(x);
+
+const y= 10;
+console.log(y);
+const z= 3;
+z=4;
+
+// due to this error 
+// nothing int the code actually runs after this 
+
+let a = 10;
+let b=4.5;
+
+let name =" shaswata";
+let isloggedin= true ;
+let c;
+let user = null;
+//  typeof returns the type of the data 
+let p = 10;//value 
+let q = "10";// string
+let r = true;// boolean 
+let d;//undefui=ined 
+let e = null;//null
+
+x = 5
+x + 2
+if (x > 3) {}
+// first 2 expression lasst is statement 
+
+let user1 = null;
+
+if (user) {
+  console.log("logged in");
+}
+
+for(let i=0;i<3;i++){
+  console.log(i);
+}
+
+let x1=3;
+while(x1>0){
+console.log(x);
+x--;
+}
