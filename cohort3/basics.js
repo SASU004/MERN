2a32d6aceb4013410d39ebf87610c115c74637e8
Shaswata Ghosh <shaswataghosh091@gmail.com>
2026-02-04 04:22:51 +0530

    added a basic js file
    some practice assisgnements for the very very simple basics of js from harkirat cohort 3

--- cohort3/basics.js
+++ cohort3/basics.js
@@ -36,3 +36,39 @@ let user={
 };
 
 greet(user);
+
+
+// Also tell the user if they are legal to vote or not
+
+function greet(user){
+  
+  let title;
+  let eli;
+  if(user.gender=="male")title="Mr";
+  else if(user.gender=="female")title="Ms";
+  else title ="Others";
+  if(user.age>18)eli="you are eligible to vote";
+  else eli="you are not eligible to vote"
+  console.log(" heelo "+title+" "+user.name+" your age is "+user.age+" so "+eli);
+}
+
+let user={
+  name:"hdfuifgsdiufg",
+  age:2,
+  gender:"male",
+};
+
+greet(user);
+
+// Write a function that takes an array of numbers as input, 
+// and returns a new array with only even values.
+// Read about filter in JS
+
+function eve(nums) {
+  return nums.filter(num => num % 2 === 0);
+}
+
+
+let nums=[1,2,3,4,5,6,7,8,9,];
+let r=eve(nums);
+console.log(r);
